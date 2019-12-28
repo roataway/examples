67284016106e88f7bb8b79d547c4b8651c566324
Vlad Drahnea <vlad.drahnea@gmail.com>
2019-12-28 21:51:33 +0200

    remove cocoaMQTT example

--- ios-swift-cocoamqtt/Pods/Target Support Files/Moscapsule/Moscapsule-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Moscapsule : NSObject
-@end
-@implementation PodsDummy_Moscapsule
-@end
--- ios-swift-cocoamqtt/Pods/Target Support Files/Pods-ios-swift-cocoamqtt/Pods-ios-swift-cocoamqtt-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ios_swift_cocoamqtt : NSObject
-@end
-@implementation PodsDummy_Pods_ios_swift_cocoamqtt
-@end
