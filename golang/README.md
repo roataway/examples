6aafeb4b5a02e501d1275b0937eadd417389e09e
tony <ayakovlenko@users.noreply.github.com>
2020-06-14 00:57:05 +0300

    Improve Go example

--- golang/README.md
+++ golang/README.md
@@ -6,4 +6,20 @@ https://golang.org/doc/install
 2. Make sure `GOPATH` is set up:  
 https://golang.org/doc/gopath_code.html#GOPATH
 
-3. Run the program `go run .`
+3. Run the program: `go run .`
+
+Example output:
+
+``` plain
+$ go run .
+Connecting to opendata.dekart.com:1945 ...
+Subscribing to telemetry/transport/+ ...
+Receiving messages, press Ctrl+C to stop
+[2020-06-25 20:41:03 +03:00] Vehicle 0000119    at (47.032436, 28.820768),  8 km/h
+[2020-06-25 20:41:03 +03:00] Vehicle 0000263    at (46.988174, 28.885153), 13 km/h
+[2020-06-25 20:41:03 +03:00] Vehicle 0000074    at (47.023495, 28.833403),  0 km/h
+[2020-06-25 20:41:02 +03:00] Vehicle 0000237    at (46.987362, 28.885031),  0 km/h
+[2020-06-25 20:41:02 +03:00] Vehicle 0000135    at (46.990456, 28.786341), 12 km/h
+[2020-06-25 20:41:03 +03:00] Vehicle 000026267  at (47.027409, 28.771643),  0 km/h
+...
+```
--- golang/main.go
+++ golang/main.go
@@ -3,30 +3,41 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"os"
 	"strings"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-type Telemetry struct {
-	Speed     int8
-	Latitude  float32
-	Longitude float32
-	Direction float32
-	Timestamp string
-	Sat       int8
+var loc *time.Location = loadLocation("Europe/Chisinau")
+
+type telemetry struct {
+	Direction float32   `json:"direction"`
+	Latitude  float32   `json:"latitude"`
+	Longitude float32   `json:"longitude"`
+	Sat       int8      `json:"sat"`
+	Speed     int8      `json:"speed"`
+	Timestamp time.Time `json:"timestamp"`
 }
 
 func onMessageReceived(client mqtt.Client, message mqtt.Message) {
 	pieces := strings.Split(message.Topic(), "/")
-	transportId := pieces[len(pieces)-1]
+	transportID := pieces[len(pieces)-1]
 
-	var telemetry Telemetry
-	if err := json.Unmarshal(message.Payload(), &telemetry); err != nil {
+	var t telemetry
+	if err := json.Unmarshal(message.Payload(), &t); err != nil {
 		panic(err)
 	}
-	fmt.Printf("Vehicle %s\tat lat: %f, lon: %f\n", transportId, telemetry.Latitude, telemetry.Longitude)
 
+	fmt.Printf(
+		"[%s] Vehicle %-10s at (%f, %f), %2d km/h\n",
+		t.Timestamp.In(loc).Format("2006-01-02 15:04:05 -07:00"),
+		transportID,
+		t.Latitude,
+		t.Longitude,
+		t.Speed,
+	)
 }
 
 func main() {
@@ -54,5 +65,12 @@ func main() {
 	fmt.Printf("Receiving messages, press Ctrl+C to stop\n")
 	for {
 	}
+}
 
+func loadLocation(name string) *time.Location {
+	loc, err := time.LoadLocation(name)
+	if err != nil {
+		panic(err)
+	}
+	return loc
 }
